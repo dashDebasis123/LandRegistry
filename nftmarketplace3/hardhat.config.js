cefa4aedc31e910e75e6411ab29e91d296dcfd62
dashDebasis123 <dash.debasisi11@gmail.com>
2023-10-28 18:21:35 +0530

    first

---
+++ nftmarketplace3/hardhat.config.js
@@ -0,0 +1,58 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const SEPOLIA_RPC_URL = "https://eth-sepolia.g.alchemy.com/v2/rhFU6gtSqgHTwV2ImBhPCREANGrsZmma"
+const PRIVATE_KEY = "0x63c1bde234ec7eea0d23064a18e4003ce9dfec4a742cbd1b8e3b58ac33969ab0"
+const ETHERSCAN_API_KEY = "GJ4JZ2CA667A5216P7E35MH7TDFQCCM6IF"
+const REPORT_GAS = true
+
+
+module.exports = {
+  solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            saveDeployments: true,
+            chainId: 11155111,
+        },
+        
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["NftMarketplace"],
+    },
+    mocha: {
+        timeout: 200000, 
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, 
+            1: 0, 
+        },
+        player: {
+            default: 1,
+        },
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["NftMarketplace"],
+    }
+}
---
+++ nftmarketplace3/scripts/mint-and-list-item.js
@@ -0,0 +1,43 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+const PRICE = ethers.parseEther("0.1")
+
+async function mintAndList() {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    // console.log(nftMarketplace)
+    const basicNft= await ethers.getContract("BasicNftTwo")
+    // const nftMarketplaceDeployment = await deployments.get("NftMarketplace");
+    // const basicNftDeployment = await deployments.get("BasicNftTwo");
+
+    // if (!nftMarketplaceDeployment || !basicNftDeployment) {
+    // throw new Error("Contract deployments not found");
+    // }
+    // else{console.log("good")}
+    console.log("Minting NFT...")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.logs[0].args.tokenId
+    console.log(tokenId)
+    console.log(nftMarketplace.target)
+    console.log("Approving NFT...")
+    
+    const approvalTx = await basicNft.approve(nftMarketplace.target, tokenId)
+    // console.log(approvalTx)
+    await approvalTx.wait(1)
+    console.log("Listing NFT...")
+    const tx = await nftMarketplace.listItem(basicNft.target, tokenId, PRICE)
+    await tx.wait(1)
+    console.log("NFT Listed!")
+    if (network.config.chainId == 31337) {
+        // Moralis has a hard time if you move more than 1 at once!
+        await moveBlocks(1, (sleepAmount = 1000))
+    }
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
